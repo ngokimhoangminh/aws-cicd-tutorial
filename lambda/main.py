ddf5f43fd4301a2f0320ab53fece474ca7c7920a
MinhNKH <hoangminhcp10@gmail.com>
2024-05-10 20:29:06 +0700

    feat/get number of accesses

--- lambda/main.py
+++ lambda/main.py
@@ -1,10 +1,36 @@
 import os
+import boto3
 
 def handler(event, context):
+    # handle event.
+    url = event["rawPath"]
+
+    if url != "/":
+        return {"statusCode": 404, "body": "Not found."}
+
+    # Get a reference to the DDB table.
+    table = _get_table()
+
+    response = table.get_item(Key={"key": "number_accesses"})
+
+    if "Item" in response:
+        number_accesses = response["Item"]["value"]
+    else:
+        number_accesses = 0
+
+    new_number_accesses = number_accesses + 1
+    table.put_item(Item={"key": "number_accesses", "value": new_number_accesses})
+
     version = os.environ.get("VERSION", "0.0")
     response_body = {
         "message": "🖖 🖖 Hello World 🖖 👋",
         "version": version,
+        "number_accesses": new_number_accesses,
     }
 
     return {"status_code": 200, "body": response_body}
+
+def _get_table():
+    table_name = os.environ.get("TABLE_NAME")
+
+    return boto3.resource("dynamodb").Table(table_name)
--- lib/aws-cicd-tutorial-stack.ts
+++ lib/aws-cicd-tutorial-stack.ts
@@ -1,7 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import * as lambda from "aws-cdk-lib/aws-lambda";
-import { Construct } from 'constructs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import * as dotenv from "dotenv";
+import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsCicdTutorialStack extends cdk.Stack {
@@ -18,14 +19,22 @@ export class AwsCicdTutorialStack extends cdk.Stack {
     // Load the environment .env file.
     dotenv.config();
 
+    const table = new dynamodb.Table(this, 'AccessTimes', {
+      partitionKey: { name: 'key', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
+
     const lambdaFunc = new lambda.Function(this, 'LambdaFunction', {
       runtime: lambda.Runtime.PYTHON_3_12,
       code: lambda.Code.fromAsset("lambda"),
       handler: 'main.handler',
       environment: {
-        VERSION: process.env.VERSION || "0.0"
+        VERSION: process.env.VERSION || "0.0",
+        TABLE_NAME: table.tableName
       }
     });
+
+    table.grantReadWriteData(lambdaFunc);
 
     const functionUrl = lambdaFunc.addFunctionUrl({
       authType: lambda.FunctionUrlAuthType.NONE,
